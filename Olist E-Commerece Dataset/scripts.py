1f30e622e821b17f2b92af8b827be030bf127199
“IamRaNo” <dasranojoy9@gmail.com>
2025-11-26 11:48:39 +0530

    structure created

--- Olist E-Commerece Dataset/scripts.py
+++ Olist E-Commerece Dataset/scripts.py
@@ -1,1 +1,28 @@
+import kagglehub
+import pandas as pd
+import os
 
+path = kagglehub.dataset_download("olistbr/brazilian-ecommerce")
+print("Path to dataset files:", path)
+
+
+for file in os.listdir(path):
+    if file.endswith(".csv"):
+        df = pd.read_csv(os.path.join(path, file))
+        print(file, df.shape)
+
+from sqlalchemy import create_engine
+
+user = 'root'
+password = '7003890541'
+port = 3306
+host = 'localhost'
+database = 'olist'
+
+engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}")
+
+for file in os.listdir(path):
+    if file.endswith(".csv"):
+        table = file.replace(".csv","")
+        df = pd.read_csv(os.path.join(path, file))
+        df.to_sql(table, engine, index=False, if_exists="replace")
--- Synthea Datasets/scripts.py
+++ Synthea Datasets/scripts.py
@@ -1,1 +1,34 @@
+import gdown
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+import pymysql
+
+folder_url = "https://drive.google.com/drive/folders/1UHH9wmZ137_IK8zTSQbWcQd7dbXYEC1c?usp=drive_link"
+download_path = "synthea_data"
+
+# download entire folder
+gdown.download_folder(folder_url, output=download_path, quiet=False)
+
+# create database synthea if not exists
+conn = pymysql.connect(host='localhost', user='root', password='7003890541')
+cur = conn.cursor()
+cur.execute("CREATE DATABASE IF NOT EXISTS synthea")
+conn.close()
+
+engine = create_engine("mysql+pymysql://root:7003890541@localhost:3306/synthea")
+
+# loop through all files and load into SQL
+for file in os.listdir(download_path):
+    if file.endswith(".csv"):
+        filepath = os.path.join(download_path, file)
+        df = pd.read_csv(filepath)
+        table = file.replace(".csv","")
+        print("Loading:", table, df.shape)
+
+        df.to_sql(table,
+                  engine,
+                  index=False,
+                  if_exists="replace",
+                  chunksize=10000)
 
